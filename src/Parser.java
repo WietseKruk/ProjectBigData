dc3b6acb5a85718cb85c669c9f89ab70d5274141
WietseKruk <wietsekruk@gmail.com>
2021-03-16 13:33:14 +0100

    episode RegEx

--- src/Parser.java
+++ src/Parser.java
@@ -7,9 +7,10 @@ public void parse(String[] lines, String type){
 
     Pattern pattern;
     type.toLowerCase();
+
     final String movieExpression = "(^.*)\\((\\d{4}|\\?{4})(/I|II|III)?\\)([ ]?)()(\\(V\\)|\\(TV\\))?\\t*(\\d{4})$";
-    final String episodeExpression = "(^\"\".\"\")((\\d{4}|\\?{4})(\\/I|II|III)?)\\{(.?)(#(\\d).(\\d))\\}\\t*(\\d{4}|\\?{4})$";
-    final String seriesExpression = "(^\"\".\"\") ((\\d{4}|?{4})(/I|II|III)?) {(.?)(#(\\d).(\\d))}\\t*(\\d{4}|?{4})$";
+    final String episodeExpression = "(^\\\".*\\\") (\\(\\d{4}\\)) (\\{.*\\})\\t*| *(\\d{4})";
+    final String seriesExpression = "(^\\\"\\\".\\\"\\\") ((\\d{4}|?{4})(/I|II|III)?) {(.?)(#(\\d).(\\d))}\\t*(\\d{4}|?{4})$";
 
     switch(type){
         case "movie":
@@ -30,7 +31,7 @@ public void parse(String[] lines, String type){
 
         while(matcher.find()){
         System.out.println(matcher.group());
-            for(int i = 0; i < matcher.groupCount(); i++){
+            for(int i = 1; i < matcher.groupCount(); i++){
                 System.out.println(matcher.group(i));
             }
         }
